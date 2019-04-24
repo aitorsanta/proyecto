b07fe8fadf8b9ae56b2d9273bf84d27a777c4b2b
aitorsanta <aitor.santa@opendeusto.es>
2019-04-24 17:22:49 +0200

    Pruebas con la base de datos remota

--- urlResponseHandlers.js
+++ urlResponseHandlers.js
@@ -69,10 +69,15 @@ exports.obtenerAlumnos = obtenerAlumnos;
 exports.obtenerTutores = obtenerTutores;
 exports.eliminarUsuario = eliminarUsuario;
 
-/*private static Connection getConnection() throws URISyntaxException, SQLException {
-    String dbUrl = System.getenv("JDBC_DATABASE_URL");
-    return DriverManager.getConnection(dbUrl);
-}*/
+private static Connection getConnection() throws URISyntaxException, SQLException {
+    URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+    return DriverManager.getConnection(dbUrl, username, password);
+}
 
 /*
 Función para logearte en la aplicación
